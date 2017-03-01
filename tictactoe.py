404f65d43e65a8ca658aecf62f1e65ed16391bb5
prabhakaran9397 <prabhakaran9397@gmail.com>
2017-03-01 12:06:47 +0530

    draw defined

--- tictactoe.py
+++ tictactoe.py
@@ -7,6 +7,7 @@ Grid = [[1, 2, 3],
 Human, Computer = O, X = 'O', 'X'
 Current = X
 Mode = 0
+Winner = "Draw!"
 
 def mode(val):
 	global Mode
@@ -73,21 +74,39 @@ def normalAlgo(openSet):
 			for j in range(3):
 				if bH[j][2-j] == 0 and j*3+2-j in openSet:
 					return j*3+2-j
-	return openSet[randint(0, len(openSet)-1)]
+	return easyAlgo(openSet)
 
 def hardAlgo(openSet):
-	pass
+	bH = booleanize(Grid, Human)
+	for i in range(3):
+		if((~(bH[i][0]^bH[i][1]^bH[i][2]))&(bH[i][0]|bH[i][1]|bH[i][2])):
+			for j in range(3):
+				if bH[i][j] == 0 and i*3+j in openSet:
+					return i*3+j
+		if((~(bH[0][i]^bH[1][i]^bH[2][i]))&(bH[0][i]|bH[1][i]|bH[2][i])):
+			for j in range(3):
+				if bH[j][i] == 0 and j*3+i in openSet:
+					return j*3+i
+
+	if((~(bH[0][0]^bH[1][1]^bH[2][2]))&(bH[0][0]|bH[1][1]|bH[2][2])):
+			for j in range(3):
+				if bH[j][j] == 0 and j*3+j in openSet:
+					return j*3+j
+	if((~(bH[0][2]^bH[1][1]^bH[2][0]))&(bH[0][2]|bH[1][1]|bH[2][0])):
+			for j in range(3):
+				if bH[j][2-j] == 0 and j*3+2-j in openSet:
+					return j*3+2-j
+	return easyAlgo(openSet)
 
 def getValComp(openSet):
+	Algo = {
+		0 : easyAlgo,
+		1 : normalAlgo,
+		2 : hardAlgo
+	}
 	print 'Computer has...'
-	if Mode == 1:
-		choice = normalAlgo(openSet)
-	elif Mode == 2:
-		choice = hardAlgo(openSet)
-	else:
-		choice = easyAlgo(openSet)
 	os.system('sleep 0.5')
-	return choice
+	return Algo[Mode](openSet)
 
 def getVal():
 	openSet = []
@@ -104,28 +123,40 @@ def getVal():
 	Grid[choice/3][choice%3] = Current
 
 def checkForWinner():
+	global Winner
 	bX = booleanize(Grid, X)
-	bO = booleanize(Grid, O)
 	if (
 		(bX[0][0]&bX[0][1]&bX[0][2]) | (bX[1][0]&bX[1][1]&bX[1][2]) | (bX[2][0]&bX[2][1]&bX[2][2]) | 
 		(bX[0][0]&bX[1][0]&bX[2][0]) | (bX[0][1]&bX[1][1]&bX[2][1]) | (bX[0][2]&bX[1][2]&bX[2][2]) | 
-		(bX[0][0]&bX[1][1]&bX[2][2]) | (bX[0][2]&bX[1][1]&bX[2][0]) |
+		(bX[0][0]&bX[1][1]&bX[2][2]) | (bX[0][2]&bX[1][1]&bX[2][0])
+	   ):
+		if Human == X:
+			Winner = "You won!"
+		else:
+			Winner = "Computer won!"
+		return False
+	bO = booleanize(Grid, O)
+	if (
 		(bO[0][0]&bO[0][1]&bO[0][2]) | (bO[1][0]&bO[1][1]&bO[1][2]) | (bO[2][0]&bO[2][1]&bO[2][2]) | 
 		(bO[0][0]&bO[1][0]&bO[2][0]) | (bO[0][1]&bO[1][1]&bO[2][1]) | (bO[0][2]&bO[1][2]&bO[2][2]) | 
 		(bO[0][0]&bO[1][1]&bO[2][2]) | (bO[0][2]&bO[1][1]&bO[2][0])
-	   ): return False
-	return True
+	   ):
+		if Human == O:
+			Winner = "You won!"
+		else:
+			Winner = "Computer won!"
+		return False
+
+	for i in range(9):
+		if type(Grid[i/3][i%3]) == int:
+			return True
+	return False
 
 
 def changeTurn():
 	global Current
 	if Current == X: Current = O
-	else: Current = X
-
-def printWinner():
-	changeTurn()
-	if Current == Human: print "You win!" 
-	else: print "Computer wins!"	
+	else: Current = X	
 
 def main():
 	choice(raw_input('X or O? '))
@@ -135,7 +166,7 @@ def main():
 		getVal()
 		changeTurn()
 	printGrid()
-	printWinner()
+	print Winner
 
 if __name__ == '__main__':
 	main()
